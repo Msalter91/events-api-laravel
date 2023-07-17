9cc9ce8b4333d086f17e5a8b7fc20425a52d0c0e
Mike Salter <mikey.l.salter@gmail.com>
2023-07-17 15:36:08 +0100

    added query param for optional relationship loading

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -9,9 +9,29 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+
+  protected function shouldIncludeRelation(string $relation): bool
+  {
+    $include = request()->query('include');
+    if(!$include) {
+      return false;
+    }
+    $relations = array_map('trim', explode(',', $include));
+
+    return in_array($relation, $relations);
+  }
+
     public function index()
-    {
-        return EventResource::collection(Event::with('user')->paginate());
+    {   
+      $query = Event::query();
+      $relations = ['user', 'attendees', 'attendees.user'];
+      foreach($relations as $relation) {
+        $query->when(
+          $this->shouldIncludeRelation($relation), 
+          fn($q) => $q->with($relation)
+        );
+      };
+        return EventResource::collection($query->latest()->paginate());
     }
 
     public function store(Request $request)
